66ae5e322366948752c82137799fe6c03aafd4da
Pranav Bykampadi <pranav.bykampadi@gmail.com>
2023-09-20 12:04:44 +0530

    final commit

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+# Google Maps API Key
+google_maps_api_key = 'AIzaSyAskxa9jT-f88ez7gOcB4ybzWGITUQoNts'
+
+# Uber API Credentials
+uber_client_id = 'lUcg8FaeYT9ODzN3a6cCxMN_X5rBz9DR'
+uber_client_secret = 'aVF8MFunfgyDiqFvRHgHwYccrk1Ei0aIY1knStx1'
+uber_server_token = 'fwDWMFunfaewda-wPL29Ddm8GJnStx1'
+
+# API endpoints
+google_maps_directions_url = 'https://maps.googleapis.com/maps/api/directions/json'
+uber_estimate_url = 'https://api.uber.com/v1.2/estimates/price'
+uber_request_url = 'https://sandbox-api.uber.com/v2/requests'
