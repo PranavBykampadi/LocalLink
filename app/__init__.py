a5f5f5aa781766fe4d1bf786a9bddd214f8e59eb
Pranav Bykampadi <pranav.bykampadi@gmail.com>
2023-09-20 11:56:36 +0530

    final commit

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from app.config import google_maps_api_key
-
-app = Flask(__name__)
-app.config.from_object('app.config')  # Corrected import path
-
-# Add any necessary extensions or configuration here
